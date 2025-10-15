db59cadfb8b96958ceef36b28399258fcdd9d712
Pedro Garcia <pedrofsgarcia.pro@gmail.com>
2025-10-15 16:31:30 -0300

    fix(init database): Corrige inicialização do banco de dados
    - Adiciona um tempo de espera antes de tentar fazer o seeding no banco de dados para que o banco esteja devidamente inicializado no docker
    - Evita erros de acesso ao banco de dados no processo de seeding

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
+import time
 from flask import Flask
 from flask_cors import CORS
+from psycopg2 import OperationalError
 from app.api import initialize_api
 from app.config import settings
 from app.containers import Container
 from app.database.seed_db import seed_db
 from app.core.logger_config import logger
-from app.models import *
+from app.models.models import *
 
 
 def create_app(container: Container | None = None) -> Flask:
@@ -31,9 +33,19 @@ def create_app(container: Container | None = None) -> Flask:
         
     db.init_app(app)
     with app.app_context():
-        # db.drop_all()
-        # db.create_all()
-        # seed_db()
-        ...
+        db.drop_all()
+        db.create_all()
+
+        # Esperar um pouco o banco estabilizar
+        for i in range(5):
+            try:
+                seed_db()
+                print("✅ Banco populado com sucesso!")
+                break
+            except OperationalError as e:
+                print(f"⏳ Tentando novamente ({i+1}/5)...")
+                time.sleep(2)
+        else:
+            print("❌ Falha ao popular o banco após várias tentativas.")
 
     return app
