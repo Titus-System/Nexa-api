6e7b63aa6b9aaa6b859033aa1b3e919da2553cd7
Eduardo Fonseca Ribeiro <eduardo10122005@gmail.com>
2025-10-16 08:37:10 -0300

    feat(api): adicionando endpoint de upload de pdf

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,6 +5,8 @@ from app.api.classification_resource import PartnumberClassification
 from app.api.health_check import CheckWebSocketConnection, HealthCheck
 from app.api.partnumber_resource import PartnumberResource
 from app.api.task_resources import TaskResource
+from app.api.upload_pdf_resource import UploadPedidoResource
+
     
 
 def initialize_api(app: Flask) -> Api:
@@ -15,5 +17,7 @@ def initialize_api(app: Flask) -> Api:
     api.add_resource(PartnumberClassification, "/classify-partnumber")
     api.add_resource(TaskResource, "/tasks")
     api.add_resource(PartnumberResource, "/partnumbers", "/partnumbers/<string:partnumber>")
+    api.add_resource(UploadPedidoResource, "/upload-pdf")
+
 
     return api
---
+++ app/api/upload_pdf_resource.py
@@ -0,0 +1,31 @@
+from flask import request
+from flask_restful import Resource
+from services.pdfExtrator import extract_part_numbers
+
+
+class UploadPedidoResource(Resource):
+    def post(self):
+        if "pedido" not in request.files:
+            return {"message": "Nenhum arquivo enviado."}, 400
+
+        file = request.files["pedido"]
+
+        if file.filename == "":
+            return {"message": "Arquivo sem nome."}, 400
+
+        if not file.filename.lower().endswith(".pdf"):
+            return {"message": "Formato inválido, apenas PDFs são aceitos."}, 400
+
+        try:
+            pdf_bytes = file.read()
+
+            part_numbers = extract_part_numbers(pdf_bytes)
+
+            return {
+                "message": "Arquivo processado com sucesso!",
+                "part_numbers": part_numbers,
+                "total": len(part_numbers)
+            }, 200
+
+        except Exception as e:
+            return {"error": str(e)}, 500
